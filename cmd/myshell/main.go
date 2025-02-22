4c3672175e70851f42c101e720240cf31c7c74b8
MogboPython <mogbo18@yahoo.com>
2025-02-22 23:32:13 +0100

    echo

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,25 +4,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
 func main() {
+	var command string
+	var args string
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Println("An error occured: ", err)
 			os.Exit(1)
 		}
-		if command == "exit 0\n" {
-			os.Exit(0)
-		}
 
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		input = strings.TrimSpace(input)
+		parts := strings.SplitN(input, " ", 2)
+		command = parts[0]
+
+		switch command {
+		case "exit":
+			os.Exit(0)
+		case "echo":
+			args = parts[1]
+			fmt.Println(args)
+		default:
+			fmt.Println(command + ": command not found")
+		}
 	}
 }
