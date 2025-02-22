44db395c8f5f910ef190dcc946845ac41f79c984
MogboPython <mogbo18@yahoo.com>
2025-02-22 22:36:26 +0100

    REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,15 +10,16 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println("An error occured: ", err)
-		os.Exit(1)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occured: ", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
